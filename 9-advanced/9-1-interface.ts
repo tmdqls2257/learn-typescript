634a8bc5461167be9d8a3fc4a0a83b6f99ff0d34
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-07-30 01:49:31 +0900

    interface vs type

---
+++ 9-advanced/9-1-interface.ts
@@ -0,0 +1,56 @@
+{type PositionType = {
+  x: number;
+  y: number;
+};
+interface PositionInterface {
+  x: number;
+  y: number;
+};
+
+// object *
+const obj1:PositionType = {
+  x: 1,
+  y: 1,
+};
+
+const obj2:PositionInterface = {
+  x: 1,
+  y: 1,
+  z: 1,
+};
+
+// class *
+class Pos1 implements PositionType{
+  x: number;
+  y: number;
+}
+class Pos2 implements PositionInterface{
+  x: number;
+  y: number;
+  z: 1;
+
+}
+
+// Extends
+interface ZPositionInterface extends PositionInterface{
+  // 기존의 Interface에 확장하고 싶을 때 사용
+}
+type ZPositionType = PositionType & {z: number};
+// 기존의 타입에 Z값을 더할 수 있습니다.
+
+// only interface can be merge
+interface PositionInterface{
+  z: number;
+}
+
+// Type aliases can use computed properties
+type Person = {
+  name:string,
+  age: number,
+}
+type Name = Person['name'];
+// person에 있는 name이라는 키가 가지고 있는 타입을 쓰게 되어 string타입이 됩니다.
+type Age = Person['age'];
+// person에 있는 age이라는 키가 가지고 있는 타입을 쓰게 되어 number타입이 됩니다.
+
+}
